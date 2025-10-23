93729ade2cf7ba3c57b5220b6aaaeee405edec7d
Михалюк Володимир bc2025-4 <Volodymyr.Mykhaliuk@lnu.edu.ua>
2025-10-23 14:08:14 +0300

    main.js

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 const { program } = require('commander');
 const http = require('http');
-const fs = require('fs').promises;
-const { XMLBuilder } = require('fast-xml-parser');
-const url = require('url');
+const fs = require('fs');
 
 program
   .requiredOption('-i, --input <path>', 'Input JSON file path')
@@ -10,36 +8,18 @@ program
   .requiredOption('-p, --port <port>', 'Server port');
 
 program.parse(process.argv);
+
 const options = program.opts();
 
-const builder = new XMLBuilder({ ignoreAttributes: false });
+// Перевірка наявності файлу
+if (!fs.existsSync(options.input)) {
+  console.error('Cannot find input file');
+  process.exit(1);
+}
 
-const server = http.createServer(async (req, res) => {
-  try {
-    const query = url.parse(req.url, true).query;
-    const data = JSON.parse(await fs.readFile(options.input, 'utf-8'));
-
-    let filtered = data;
-
-    if (query.min_rainfall) {
-      const minRain = parseFloat(query.min_rainfall);
-      filtered = filtered.filter(d => d.Rainfall > minRain);
-    }
-
-    const result = filtered.map(item => ({
-      rainfall: item.Rainfall,
-      pressure3pm: item.Pressure3pm,
-      ...(query.humidity === 'true' ? { humidity: item.Humidity3pm } : {})
-    }));
-
-    const xml = builder.build({ weather_data: { record: result } });
-
-    res.writeHead(200, { 'Content-Type': 'application/xml' });
-    res.end(xml);
-  } catch (err) {
-    res.writeHead(500, { 'Content-Type': 'text/plain' });
-    res.end('Server error: ' + err.message);
-  }
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Server is running');
 });
 
 server.listen(options.port, options.host, () => {
